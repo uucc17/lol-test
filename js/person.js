c5cab31f48180413e73523fd47edab0a9b5a4175
uucc17 <w2213@e-mirim.hs.kr>
2024-03-13 23:39:22 +0900

    기본 틀 수정, 사람 참여 수 기능

---
+++ js/person.js
@@ -0,0 +1,17 @@
+// 참여자 수 초기화
+let participantCount = 0;
+
+// 참여자 수 업데이트 함수
+function updateParticipantCount() {
+    const countElement = document.getElementById('personP');
+    countElement.textContent = participantCount + "명이 참여했습니다.";
+}
+
+// 참여 버튼 이벤트 리스너
+document.querySelector('.startBtn').addEventListener('click', function() {
+    participantCount++;
+    updateParticipantCount();
+});
+
+// 초기 참여자 수 업데이트
+updateParticipantCount();
