844341c7409976e9726ec5817b65d1659ea18fe6
uucc17 <w2213@e-mirim.hs.kr>
2024-03-14 20:37:42 +0900

    input => button 부트스트랩으로 버튼 디자인, 위치 수정

--- js/person.js
+++ js/person.js
@@ -4,7 +4,7 @@ let participantCount = 0;
 // 참여자 수 업데이트 함수
 function updateParticipantCount() {
     const countElement = document.getElementById('personP');
-    countElement.textContent = participantCount + "명이 참여했습니다.";
+    countElement.textContent = participantCount + " 명이 참여했습니다.";
 }
 
 // 참여 버튼 이벤트 리스너
