a465ae899d23b56ea7cbd4f251b0b3ecaaa4a676
uucc17 <w2213@e-mirim.hs.kr>
2024-03-15 20:00:44 +0900

    q&a 엘리멘트 선언하기 추가 수정

---
+++ js/q&a.js
@@ -0,0 +1,8 @@
+let queryElement = document.getElementById('queryP');
+let ImageElement = document.getElementById('img_');
+let answerElement_A = document.getElementById('Atn_1');
+let answerElement_B = document.getElementById('Btn_2');
+
+const askring = [
+    
+]
